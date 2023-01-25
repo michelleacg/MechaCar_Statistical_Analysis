c2ebc87e83834c6eff067bb3042794aab5995495
michelleacg <111101012+michelleacg@users.noreply.github.com>
2023-01-25 14:52:43 -0800

    commit

--- Challenge/MechaCarChallenge.R
+++ Challenge/MechaCarChallenge.R
@@ -3,11 +3,15 @@ install.packages("jsonlight")
 install.packages("dplyr")
 library(tidyverse)
 
+#Part 1: Linear Regression to Predict MPG
+
 df_MechaCar <- read.csv(file="MechaCar_mpg.csv")
 lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,data=df_MechaCar)
 summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,data=df_MechaCar))
 
-#
+#Part 2: Create Visualizations for the Trip Analysis
+
+#Technical Analysis
 df_Coil <- read.csv(file="Suspension_Coil.csv")
 summary_coil <- df_Coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),STD=sd(PSI),.groups="keep")
 summary_Lot <- df_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),STD=sd(PSI),.groups="keep")
@@ -15,9 +19,7 @@ summary_Lot <- df_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(P
 
 
 
-#3
-
-#we are going to do a t test in the entire lot and then we are going to do an entire test in the sub set
+#Part 3: T-Tests on Suspension Coils
 
 t.test(df_Coil$PSI,mu=1500)
 
